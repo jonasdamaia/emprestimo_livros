21ad883edec66450e99871a854b6a2b1219cd03d
Jonas da Maia <78278603+jonasdamaia@users.noreply.github.com>
2022-08-12 14:46:53 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		Emprestimo emprestimo = new Emprestimo();
-		// System.out.println(emprestimo.getPosicaoLivro());
 		emprestimo.iniciar();
 
 	}

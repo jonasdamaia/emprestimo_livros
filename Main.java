2b346d61e0a9f42b7a08931553444a76ef67e237
Jonas da Maia <78278603+jonasdamaia@users.noreply.github.com>
2022-08-12 13:54:09 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package emprestimos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Emprestimo emprestimo = new Emprestimo();
+		//System.out.println(emprestimo.getPosicaoLivro());
+		emprestimo.iniciar();
+
+	}
+}
